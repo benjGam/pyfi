c4c03ce3f7dc4bb22ee4a9e5a21b5b095a205052
benjGam <mecopilive@gmail.com>
2025-09-26 20:26:31 +0200

    feat(Directory): implement base init logic

--- src/directory.py
+++ src/directory.py
@@ -1,7 +1,14 @@
 from src.systorage import Systorage
-from src.path import Path
+from src.file import File
+from __future__ import annotations
+import os
 
 class Directory(Systorage):
     
+    _files: list[File] = []
+    _directories: list[Directory] = []
+
     def __init__(self, path: str):
         super().__init__(path)
+        if self.exists() and not os.path.isdir(path):
+            raise Exception(f"\"{path}\" is not a folder.")
