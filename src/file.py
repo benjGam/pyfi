b965610472f16d3fc9593c8daddfd091d23a7f9e
benjGam <mecopilive@gmail.com>
2025-09-26 14:46:58 +0200

    feat(File): implement 'get_name' to get it with or without extension

--- src/file.py
+++ src/file.py
@@ -9,3 +9,6 @@ class File(Systorage):
     def get_extension(self):
         path = self._path._path_literal
         return path[path.rfind("."):len(path)]
+    
+    def get_name(self, with_extension: bool = False):
+        return super().get_name() + (self.get_extension() if with_extension else "")
