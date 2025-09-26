f6f30244f9d2a606a0fdf8ed699d2bae695c6e06
benjGam <mecopilive@gmail.com>
2025-09-26 14:25:27 +0200

    feat(File): implement 'get_extension'

--- src/file.py
+++ src/file.py
@@ -1,6 +1,11 @@
 from src.systorage import Systorage
 
 class File(Systorage):
-    
+    _extension: str
+
     def __init__(self, path: str):
         super().__init__(path)
+
+    def get_extension(self):
+        path = self._path._path_literal
+        return path[path.rfind("."):len(path)]
