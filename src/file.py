c75e57f8fe01acc55f4134decdd10b813f79843d
benjGam <mecopilive@gmail.com>
2025-09-26 16:50:30 +0200

    feat(File): add explicit params & returns types, add bool return for data deletion related methods

--- src/file.py
+++ src/file.py
@@ -7,33 +7,36 @@ class File(Systorage):
     def __init__(self, path: str):
         super().__init__(path)
 
-    def get_extension(self):
+    def get_extension(self) -> str:
         path = self.get_path()
         return path[path.rfind("."):len(path)]
     
-    def get_name(self, with_extension: bool = False):
+    def get_name(self, with_extension: bool = False) -> str:
         return super().get_name() + (self.get_extension() if with_extension else "")
     
-    def create(self):
+    def create(self) -> bool:
         self.get_path_object().touch(exist_ok=True)
+        return self.get_path_object().exists()
 
-    def append(self, text_to_append):
+    def append(self, text_to_append: str) -> None:
         if not self.get_path_object().exists():
             self.create()
         with open(self.get_path(), "a") as fs:
             fs.write(text_to_append)
 
-    def write(self, text_to_write):
+    def write(self, text_to_write: str) -> None:
         if not self.get_path_object().exists():
             self.create()        
         with open(self.get_path(), "w") as fs:
             fs.write(text_to_write)
 
-    def delete_content(self):
-        if not self.get_path_object().exists(): raise
+    def delete_content(self) -> bool:
+        if not self.get_path_object().exists(): return False
         self.write("")
+        return True
 
-    def delete(self, delete_content:bool = False):
-        if not self.get_path_object().exists(): raise
+    def delete(self, delete_content:bool = False) -> bool:
+        if not self.get_path_object().exists(): return False
         if delete_content: self.delete_content()
         os.remove(self.get_path())
+        return self.get_path_object().exists() == False
