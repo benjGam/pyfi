047e14a893e5731fda1afc557ffb607eb50fa040
benjGam <mecopilive@gmail.com>
2025-09-26 06:08:17 +0200

    feat(path): implement 'exits' method

--- src/path.py
+++ src/path.py
@@ -1,8 +1,16 @@
+from pathlib import Path as path
+import os.path as syspath 
+
 class Path:
     _path_literal: str
+    _internal_path: path
 
     def __init__(self, path_literal: str):
         self._path_literal = self.format_literal_path(path_literal)
+        self._internal_path = path(self._path_literal)
+
+    def exists(self):
+        return syspath.exists(self._path_literal)
 
     ### Utils methods
 
