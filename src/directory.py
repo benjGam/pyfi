d8febcfc1b27cef8e9063260aaf73e4ddb4bdb0e
benjGam <mecopilive@gmail.com>
2025-09-27 04:03:50 +0200

    feat(Directory): implement 'load' method & autoload mecanism

--- src/directory.py
+++ src/directory.py
@@ -1,5 +1,6 @@
 from src.systorage import Systorage
 from src.file import File
+from src.path import Path
 from __future__ import annotations
 import os
 
@@ -8,7 +9,12 @@ class Directory(Systorage):
     _files: list[File] = []
     _directories: list[Directory] = []
 
-    def __init__(self, path: str):
+    def __init__(self, path: str, load: bool = False):
         super().__init__(path)
         if self.exists() and not os.path.isdir(path):
             raise Exception(f"\"{path}\" is not a folder.")
+        if load: self.load()
+
+    def load(self) -> None:
+        self._files = Path(self.get_path()).get_files()
+        self._directories = Path(self.get_path()).get_directories()
