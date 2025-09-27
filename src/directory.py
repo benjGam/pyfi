f2b6d9d699395952997ce1d956e5d78d547673cc
benjGam <mecopilive@gmail.com>
2025-09-27 04:09:10 +0200

    feat(Directory): re-implement auto_load mechanism at a different layer, to be able to 'bootstrap' exploring a full root recursively

--- src/directory.py
+++ src/directory.py
@@ -9,12 +9,12 @@ class Directory(Systorage):
     _files: list[File] = []
     _directories: list[Directory] = []
 
-    def __init__(self, path: str, load: bool = False):
+    def __init__(self, path: str, auto_load: bool = False):
         super().__init__(path)
         if self.exists() and not os.path.isdir(path):
             raise Exception(f"\"{path}\" is not a folder.")
-        if load: self.load()
+        if auto_load: self.load(True)
 
-    def load(self) -> None:
+    def load(self, recursive_load: bool = False) -> None:
         self._files = Path(self.get_path()).get_files()
-        self._directories = Path(self.get_path()).get_directories()
+        self._directories = Path(self.get_path()).get_directories(recursive_load)
