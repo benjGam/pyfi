b26c845c7a95d0888f905dbd10c0ac8c607d1d47
benjGam <mecopilive@gmail.com>
2025-09-28 02:49:42 +0200

    feat(Utils): implement 'convert_enum_values_to_str'

--- pyfile/utils.py
+++ pyfile/utils.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def flatten(base_list: list):
     result = []
     for element in base_list:
@@ -6,3 +8,6 @@ def flatten(base_list: list):
         else:
             result.append(element)
     return result
+
+def convert_enum_values_to_str(enum_values: list[Enum | str]):
+    return [x.value if type(x) != str else x for x in enum_values]
