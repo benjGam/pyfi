7320bf17eb32dc399027331dc9b9e10fa2d0a9b5
benjGam <mecopilive@gmail.com>
2025-09-30 05:15:52 +0200

    feat(Systorage): add base of 'systorage.py'

---
+++ pyfile/systorage.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from .path import Path
+
+
+class Systorage(ABC):
+
+    __path: Path
+    __name: str
+    __parent = None
+
+    def __init(self, path):
+        self.__path = Path(path)
