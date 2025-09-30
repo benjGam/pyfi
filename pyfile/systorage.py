a5468d4e3000a265e1b7112365e4773ca965a1b1
benjGam <mecopilive@gmail.com>
2025-09-30 05:29:30 +0200

    feat(Systorage): implement 'rename' method

--- pyfile/systorage.py
+++ pyfile/systorage.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from .path import Path
+import os
 
 
 class Systorage(ABC):
@@ -21,3 +22,9 @@ class Systorage(ABC):
     @abstractmethod
     def delete(self) -> bool:
         pass
+
+    def rename(self, new_name: str) -> bool:
+        old_path = self.__path.get_literal()
+        new_path = f"{str(self.__path.get_complex().parent())}/{new_name}"
+        os.rename(self.__path._literal, new_path)
+        return os.path.exists(old_path) == False and os.path.exists(new_path) == True
