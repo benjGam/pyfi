c71759eb768a75a39010eb28796dfc7b5fc28f3f
benjGam <mecopilive@gmail.com>
2025-10-01 10:58:37 +0200

    feat(Directory): implement 'create' method

--- pyfile/directory.py
+++ pyfile/directory.py
@@ -8,3 +8,7 @@ class Directory(Systorage):
         super().__init__(path)
         if not os.path.isfile(path):
             raise Exception(f'"{path}" is not a folder')
+
+    def create(self) -> bool:
+        self.__path.get_complex().mkdir(parents=True, exist_ok=True)
+        return self.exists()
