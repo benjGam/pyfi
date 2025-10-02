02433ed3cd61738258a7671acd00ec128906600f
benjGam <mecopilive@gmail.com>
2025-10-02 13:26:15 +0200

    feat(Directory): implement 'load' method

--- pyfile/directory.py
+++ pyfile/directory.py
@@ -14,6 +14,15 @@ class Directory(Systorage):
         if not os.path.isfile(path):
             raise Exception(f'"{path}" is not a folder')
 
+    def __load(self, recursive_load: bool):
+        complex_path = self.__path.get_complex()
+        child_paths = complex_path.glob("*")
+        self.__files = list(filter(lambda path: os.path.isfile(path), child_paths))
+        self.__directories = list(filter(lambda path: os.path.isdir(path), child_paths))
+        if recursive_load:
+            for directory in self.__directories:
+                directory.__load(recursive_load)
+
     def create(self) -> bool:
         self.__path.get_complex().mkdir(parents=True, exist_ok=True)
         return self.exists()
