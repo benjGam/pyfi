4333c0e29d371f9eae263640c221e8cbdf74247e
benjGam <mecopilive@gmail.com>
2025-10-06 03:50:11 +0200

    feat(SegmentedSearchResult): implement 'to_paths' method

--- pyfile/utils/segmented_seach_result.py
+++ pyfile/utils/segmented_seach_result.py
@@ -10,3 +10,10 @@ class SegmentedSearchResult:
     def __init__(self, parent: Directory, childs: list[File] | list[Directory]):
         self._parent = parent
         self._childs = childs
+
+    def to_paths(self) -> dict[str, list[str]]:
+        return {
+            self._parent.get_path(): list(
+                map(lambda child: child.get_path(), self._childs)
+            ),
+        }
