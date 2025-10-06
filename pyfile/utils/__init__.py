4c9a0e34a142e75260d7ff5f249da26adcb57065
benjGam <mecopilive@gmail.com>
2025-10-06 02:34:15 +0200

    chore: remove unused 'flatten' method

--- pyfile/utils/__init__.py
+++ pyfile/utils/__init__.py
@@ -1,3 +1,2 @@
 from .search_options import SearchOptions
 from .segmented_seach_result import SegmentedSearchResult
-from .utils import flatten
--- pyfile/utils/utils.py
+++
@@ -1,8 +0,0 @@
-def flatten(base_list: list):
-    result = []
-    for element in base_list:
-        if type(element) == list:
-            result.extend(flatten(element))
-        else:
-            result.append(element)
-    return result
